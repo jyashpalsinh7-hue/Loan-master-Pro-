239eac37b1844ee2cc597d9101e2917f0b0591d1
jyashpalsinh7-hue <jyashpalsinh7@gmail.com>
2026-06-19 08:54:59 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,20 +4,6 @@ plugins {
     alias(libs.plugins.googleDevtoolsKsp) apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
