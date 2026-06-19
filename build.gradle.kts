9d152c09fb63a4b219af9896350e136241e1e90c
jyashpalsinh7-hue <jyashpalsinh7@gmail.com>
2026-06-19 08:33:30 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,29 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.plugin.compose) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+}
+
+// Optional: If you're using Roborazzi or Secrets plugin, keep them
+// alias(libs.plugins.roborazzi) apply false
+// alias(libs.plugins.secrets) apply false
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
