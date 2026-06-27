c476095ca7a967f4cb99ef9055b3693b83f8e111
jyashpalsinh7-hue <jyashpalsinh7@gmail.com>
2026-06-27 22:36:37 +0530

    refactor: update branding and streamline assets
    - Update applicationId to com.aistudio.loanmaster.v3
    - Redesign app icon assets
    - Remove monochrome icon support
    - Remove obsolete build-debug-apk workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.loanmaster.v2fresh"
+    applicationId = "com.aistudio.loanmaster.v3"
     minSdk = 26
     targetSdk = 36
     versionCode = 1
